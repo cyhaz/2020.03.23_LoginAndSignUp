f9db151d2c606d8c1d242c7d54ceec71d715d560
cyhaz <cyhaz@naver.com>
2020-03-23 16:03:01 +0900

    회원가입 실패시 토스트 띄우기

--- app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/SignUpActivity.java
+++ app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/SignUpActivity.java
@@ -6,7 +6,9 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import kr.co.youhyun.a20200323_loginandsignup.databinding.ActivitySignUpBinding;
@@ -41,6 +43,33 @@ public class SignUpActivity extends BaseActivity {
                     @Override
                     public void onResponse(JSONObject json) {
                         Log.d("회원가입", json.toString());
+
+//                        응용문제 : 회원가입에 성공 -> "이름"님 환영합니다! 토스트 출력
+//                        실패? 서버에서 주는 message를 그대로 출력
+
+                        try {
+                            int code = json.getInt("code");
+                            if (code == 200) {
+                                // 회원가입 성공
+                                JSONObject data = json.getJSONObject("data");
+                                JSONObject user = data.getJSONObject("user");
+
+
+                            } else {
+                                // 회원가입 실패
+                                final String message = json.getString("message");
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
                     }
                 });
 
