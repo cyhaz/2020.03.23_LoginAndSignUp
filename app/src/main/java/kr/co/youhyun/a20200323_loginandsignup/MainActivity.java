6331f769329e1a0f3a1eef167c43cca5d905ddf6
cyhaz <cyhaz@naver.com>
2020-03-23 17:55:28 +0900

    사용자 정보 실제로 받아와서 출력

--- app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/MainActivity.java
+++ app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/MainActivity.java
@@ -5,9 +5,11 @@ import android.util.Log;
 
 import androidx.databinding.DataBindingUtil;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import kr.co.youhyun.a20200323_loginandsignup.databinding.ActivityMainBinding;
+import kr.co.youhyun.a20200323_loginandsignup.datas.User;
 import kr.co.youhyun.a20200323_loginandsignup.utils.ServerUtil;
 
 public class MainActivity extends BaseActivity {
@@ -35,8 +37,30 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onResponse(JSONObject json) {
                 Log.d("내정보", json.toString());
+
+                try {
+                    int code = json.getInt("code");
+                    if (code == 200) {
+                        JSONObject data = json.getJSONObject("data");
+                        JSONObject user = data.getJSONObject("user");
+                        final User myinfo = User.getUserFromJson(user);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                binding.nameTxt.setText(myinfo.getName());
+                                binding.memoTxt.setText(myinfo.getMemo());
+                                binding.phoneTxt.setText(myinfo.getPhone());
+                            }
+                        });
+                    } else {
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        });
+       });
 
     }
 }
