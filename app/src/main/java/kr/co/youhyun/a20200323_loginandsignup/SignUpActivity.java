537ddcf6962c4bf795f1f728f72dcd1c36bc099d
cyhaz <cyhaz@naver.com>
2020-03-23 15:49:52 +0900

    회원가입 서버 전달까지  => DB에서 가입 성공 확인  => UI 반영은 안한 상태 (Log로만 확인)

--- app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/SignUpActivity.java
+++ app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/SignUpActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONObject;
 
 import kr.co.youhyun.a20200323_loginandsignup.databinding.ActivitySignUpBinding;
+import kr.co.youhyun.a20200323_loginandsignup.utils.ServerUtil;
 
 public class SignUpActivity extends BaseActivity {
 
@@ -21,6 +26,26 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                아이디, 비번, 이름, 번호를 따와서 서버로 전달 => 회원가입 API에 전달
+                String inputId = binding.idEdt.getText().toString();
+                String inputPw = binding.pwEdt.getText().toString();
+                String inputName = binding.nameEdt.getText().toString();
+                String inputPhone = binding.phoneEdt.getText().toString();
+
+                ServerUtil.putRequestSignUp(mContext, inputId, inputPw, inputName, inputPhone, new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+                        Log.d("회원가입", json.toString());
+                    }
+                });
+
+            }
+        });
 
     }
 
--- app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/utils/ServerUtil.java
+++ app/src/main/java/kr/co/youhyun/a20200323_loginandsignup/utils/ServerUtil.java
@@ -83,4 +83,63 @@ public class ServerUtil {
             }
         });
     }
+
+//    파라미터 기본구조 : 어떤화면에서? context 어떤 응답 처리를 할지? handler 를 변수로
+//    가운데 추가 고려 : 화면에서 어떤 데이터를 받아서 => 서버로 전달?
+    public static void putRequestSignUp(Context context, String id, String pw, String name, String phoneNum, final JsonResponseHandler handler) {
+
+//        클라이언트 역할 수행 변수 생성
+        OkHttpClient client = new OkHttpClient();
+
+//        어느 주소(호스트주소/기능주소)로 갈지? String 변수로 저장
+        String urlstr = String.format("%s/auth", BASE_URL);
+
+//        어떤 데이터를 담어야 하는지? API 명세 참조
+        FormBody formData = new FormBody.Builder()
+                .add("login_id", id)
+                .add("password", pw)
+                .add("name", name)
+                .add("phone", phoneNum)
+                .build();
+
+//        어떤 메쏘드를 쓰는지? -> Put 메쏘드
+        Request request = new Request.Builder()
+                .url(urlstr)
+                .put(formData)
+                .build();
+        // 필요한 경우 헤더도 추가해야한다.
+
+
+//        건들면 안되는 부분
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+//                연결에 실패했을 때의 처리
+                Log.e("서버연결실패", "연결안됨!");
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+//                연결에 성공해서 응답이 돌아왔을 때의 처리 => string()으로 변환
+                String body = response.body().string();
+                Log.d("로그인응답!!!!!!", body);
+
+//                응답 내용을 JSON 객체로 가공
+                try {
+//                    body의 String을 JSONObject 형태로 변환
+//                    양식에 맞지않는 내용이면, 앱이 터질 수 있으니 try / catch로 감싸도록 처리
+                    JSONObject json = new JSONObject(body);
+
+//                    이 JSON에 대한 분석은 화면단에 넘겨주자
+                    if (handler != null) {
+                        handler.onResponse(json);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
